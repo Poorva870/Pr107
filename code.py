da8b6d069ff13f27a9e8c6a561443bc3f1851b72
Poorva870 <80689366+Poorva870@users.noreply.github.com>
2022-07-21 12:41:41 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import csv
+import plotly.express as px
+
+df = pd.read_csv("data.csv")
+
+print(df.groupby("level")["attempt"].mean())
+
+fig = px.scatter(
+            df,
+            x=df.groupby("level")["attempt"].mean(),
+            y=['Level 1', 'Level 2', 'Level 3', 'Level 4'],
+            orientation='h'
+            )
+
+fig.show()
